d1ff5628a8d70b5d873a24d5da8726b2bfdc9b57
eRoblesMorales <erobles@anzen.com.mx>
2018-10-22 15:26:20 -0500

    UPDATE-CAMBIO EN LA CARGA INICIAL

--- src/main/java/mx/gob/segob/dgtic/business/service/impl/CargaInicialServiceImpl.java
+++ src/main/java/mx/gob/segob/dgtic/business/service/impl/CargaInicialServiceImpl.java
@@ -3,16 +3,18 @@ package mx.gob.segob.dgtic.business.service.impl;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.jfree.util.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import mx.gob.segob.dgtic.business.rules.catalogo.CargaInicialRules;
 import mx.gob.segob.dgtic.business.rules.catalogo.UnidadAdministrativaRules;
+import mx.gob.segob.dgtic.business.rules.catalogo.UsuarioPerfilRules;
 import mx.gob.segob.dgtic.business.rules.catalogo.UsuarioRules;
 import mx.gob.segob.dgtic.business.service.CargaInicialService;
+import mx.gob.segob.dgtic.comun.sicoa.dto.PerfilDto;
 import mx.gob.segob.dgtic.comun.sicoa.dto.UnidadAdministrativaDto;
 import mx.gob.segob.dgtic.comun.sicoa.dto.UsuarioDto;
+import mx.gob.segob.dgtic.comun.sicoa.dto.UsuarioPerfilDto;
 import mx.gob.segob.dgtic.comun.sicoa.dto.UsuarioUnidadAdministrativaDto;
 @Service
 public class CargaInicialServiceImpl  implements CargaInicialService{
@@ -20,15 +22,22 @@ public class CargaInicialServiceImpl  implements CargaInicialService{
 	@Autowired CargaInicialRules cargaInicialRules;
 	@Autowired UsuarioRules usuarioRules;
 	@Autowired UnidadAdministrativaRules unidadAdministrativaRules;
+	@Autowired UsuarioPerfilRules usuarioPerfilRules;
 	
 	@Override
 	public void cargaInicial() {
 		List<UsuarioDto> listaUsuariosRecuperados = new ArrayList<UsuarioDto>();
 		listaUsuariosRecuperados = cargaInicialRules.recuperarUsuariosCargaInicial();
+		PerfilDto perfilDto = new PerfilDto();
+		perfilDto.setClavePerfil("3");
 		for(UsuarioDto usuarioDto: listaUsuariosRecuperados){
 			usuarioRules.agregaUsuario(usuarioDto);
+			UsuarioPerfilDto usuarioPerfilDto= new UsuarioPerfilDto();
+			usuarioPerfilDto.setClavePerfil(perfilDto);
+			usuarioPerfilDto.setClaveUsuario(usuarioDto);
 			UsuarioUnidadAdministrativaDto usuarioUnidadAdministrativaDto = new UsuarioUnidadAdministrativaDto();
 			usuarioUnidadAdministrativaDto.setClaveUsuario(usuarioDto);
+			usuarioPerfilRules.agregaUsuarioPerfil(usuarioPerfilDto);
 			
 			UnidadAdministrativaDto unidadAdministrativaDto = new UnidadAdministrativaDto();
 			unidadAdministrativaDto.setIdUnidad(1);
