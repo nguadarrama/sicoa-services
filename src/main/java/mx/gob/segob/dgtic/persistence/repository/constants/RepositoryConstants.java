07b7f580e509cd8ede300ce90e8f785c2b0412fe
Ulises Leyva <ulises.leyva.hdz@me.com>
2018-11-09 16:25:08 -0600

    Paquete de constantes para el repo

---
+++ src/main/java/mx/gob/segob/dgtic/persistence/repository/constants/RepositoryConstants.java
@@ -0,0 +1,51 @@
+package mx.gob.segob.dgtic.persistence.repository.constants;
+
+import javax.resource.spi.IllegalStateException;
+
+public class RepositoryConstants {
+	
+	private RepositoryConstants () throws IllegalStateException {
+		throw new IllegalStateException("Constants class");
+	}
+	
+	public static final String ID_ARCHIVO = "id_archivo";
+	public static final String ID_ARCHIVO2 = "idArchivo";
+	public static final String NOMBRE = "nombre";
+	public static final String URL = "url";
+	public static final String SIZE = "size";
+	public static final String ACTIVO = "activo";
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+//	public static final String
+	
+
+}
