73ee5dd181755a29a38736542577e7efb41aba86
Nadia <nguadarrama@anzen.com.mx>
2018-11-15 13:21:46 -0600

    Dash

--- src/main/java/mx/gob/segob/dgtic/business/service/impl/DashServiceImpl.java
+++ src/main/java/mx/gob/segob/dgtic/business/service/impl/DashServiceImpl.java
@@ -2,9 +2,12 @@ package mx.gob.segob.dgtic.business.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 import mx.gob.segob.dgtic.business.rules.catalogo.DashRules;
+import mx.gob.segob.dgtic.business.rules.catalogo.UsuarioRules;
 import mx.gob.segob.dgtic.business.service.DashService;
 import mx.gob.segob.dgtic.comun.sicoa.dto.DashBoardDto;
+import mx.gob.segob.dgtic.comun.sicoa.dto.UsuarioDto;
 
 @Component
 public class DashServiceImpl implements DashService {
@@ -12,10 +15,14 @@ public class DashServiceImpl implements DashService {
 	
 	@Autowired
 	private DashRules dashRules;
+	
+	@Autowired
+	private UsuarioRules usuarioRules;
 
 	@Override
 	public DashBoardDto dashBoard(Integer idUsuario) {
-		return dashRules.dashBoard(idUsuario);
+		UsuarioDto user = usuarioRules.buscaUsuarioPorId(idUsuario);
+		return dashRules.dashBoard(idUsuario, user.getClaveUsuario());
 	}
 
 }
